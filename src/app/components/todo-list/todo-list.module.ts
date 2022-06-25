b992e03dfc43db6dace8b7abd6e993a660adb793
Hunter <fffarmer@protonmail.com>
2022-06-25 14:21:42 -0400

    changed app html structure

--- src/app/components/todo-list/todo-list.module.ts
+++ src/app/components/todo-list/todo-list.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoListRoutingModule } from './todo-list-routing.module';
+import { TodoListComponent } from './todo-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoListComponent
+  ],
   imports: [
     CommonModule,
     TodoListRoutingModule
