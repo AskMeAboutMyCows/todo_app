11dd83ef0dbc7ccdc3ff04ce096858492a9ae868
Hunter <fffarmer@protonmail.com>
2022-06-25 18:29:25 -0400

    yee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoListRoutingModule } from './components/todo-list/todo-list-routing.module';
 
 const routes: Routes = [
   { path: 'todo', component: TodoListComponent }
 ];
 
 @NgModule({
+
+  declarations: [
+    TodoListComponent
+  ],
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
